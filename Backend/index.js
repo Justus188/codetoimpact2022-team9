15515196854e10445b5a46a94a6eddcc42d579cb
Justus188 <justus_ng@yahoo.com.sg>
2022-09-17 16:28:09 +0800

    Valuations dummy responses

--- Backend/index.js
+++ Backend/index.js
@@ -3,14 +3,14 @@ import bodyParser from 'body-parser'
 import dotenv from "dotenv"
 
 import instruments from './src/routes/instruments.route.js'
-//import valuations from './src/valuations.route.js'
-//import transactions from './src/transactions.route.js'
-//import investments from './src/investments.route.js'
+import valuations from './src/routes/valuations.route.js'
+//import transactions from './src/routes/transactions.route.js'
+//import investments from './src/routes/investments.route.js'
 
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', (request, response) => {response.json({info: 'Node.js, Express, Postgres API'})})
 
 app.use('/api/instruments', instruments)
-//app.use('/api/market-values', valuations)
+app.use('/api/market-values', valuations)
 //app.use('/api/transactions', transactions)
 //app.use('/api/investments', investments)
 //app.use('/api/analytics', analytics)
--- Backend/src/controllers/valuations.controller.js
+++ Backend/src/controllers/valuations.controller.js
@@ -1,28 +1,36 @@
 import pool from '../db/querypooler.js'
 
 export default class ValuationsController {
-    static async GetMarketValues(req, res, next){
-        let query = "SELECT * FROM valuations" + res.body.marketValues.reduce((segment, ))
-        try {
-            const results = await pool.query(query, params)
-        } catch (err) {
-            console.log(err.stack)
-            // res.json()
-        }
-        console.log(results.rows[0])
-        res.status(200).json(results.rows)
-    }
-
     static async AddMarketValues(req, res, next){
         let query = "INSERT INTO valuations (instrumentId, marketValue, marketValueDate) VALUES "
-        query += res.body.marketValues.reduce((total, obj) => total + ", (" + res.params.id + ", " + obj.value + ", ", obj.date + ")", "")
-        try {
-            const results = await pool.query(query)
-        } catch (err) {
-            console.log(err.stack)
+        //query += res.body.marketValues.reduce((total, obj) => total + ", (" + res.params.id + ", " + obj.value + ", ", obj.date + ")", "")
+        //try {
+        //    const results = await pool.query(query)
+        //} catch (err) {
+        //    console.log(err.stack)
             // res.json()
-        }
-        console.log(results.rows[0])
-        res.status(200).json(results.rows)
+        //}
+        //console.log(results.rows[0])
+        //res.status(200).json(results.rows)
+        res.status(200).json({
+            instrumentId: req.params.id,
+            marketValues: req.body.marketValues
+        })
+    }
+
+    static async GetMarketValues(req, res, next){
+        //try {
+        //    let query = "SELECT * FROM valuations" + res.body.marketValues.reduce((segment, ))
+        //    const results = await pool.query(query, params)
+        //} catch (err) {
+        //    console.log(err.stack)
+            // res.json()
+        //}
+        //console.log(results.rows[0])
+        //res.status(200).json(results.rows)
+        res.status(200).json({
+            instrumentId: req.params.id,
+            marketValues: [{"value": 500000000, "date": "2022-01-01"}, {"value": 600000000, "date": "2022-02-01"}]
+        })
     }
 }
